557515d6ec439025a3e54d05caa55aac4dc83afe
Zecharias Mengis <zmengis@gmail.com>
2021-06-02 15:02:30 -0500

    rolled back

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_193529) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cards", force: :cascade do |t|
-    t.string "name"
-    t.string "ability"
-    t.integer "ability_strength"
-    t.integer "hp"
-    t.string "rarity"
-    t.string "holographic"
-    t.string "image"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "collections", force: :cascade do |t|
-    t.integer "card_id"
-    t.integer "favorite"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
